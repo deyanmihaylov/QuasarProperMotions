81542e9893cc2149b0f43e81bfe4a7d6fe0618b6
cjm96 <cjm96@users.noreply.github.com>
2019-08-09 11:02:01 +0100

    What data do we want to analyse?

--- Cluster/main.py
+++ Cluster/main.py
@@ -6,6 +6,28 @@ import cpnest.model
 Lmax = 4
 
 
+# pm data to analyse:
+#  (1) - mock dipole (? stars)
+#  (2) - mock GW quad patter (? stars)
+#  (3) - real type2 (~1e3 stars)
+#  (4) - real type3 (~1e5 stars)
+#  (5) - real type2+type3 (~1e5 stars)
+dataset = 1
+if dataset==1:
+    pass
+if dataset==2:
+    pass
+if dataset==3:
+    pass
+if dataset==4:
+    pass
+if dataset==5:
+    pass
+else:
+    raise ValueError('Unknown dataset {}'.format(dataset))
+
+
+
 class VSHmodel(Model):
     """
     Vector Spherical Harmonic (VSH) fit to proper motions
