a33fc643fda84ef6aa875b60addb156b7fc075d4
cjm96 <cjm96@users.noreply.github.com>
2019-08-09 09:47:30 +0100

    Created folder for code to run on cluster

---
+++ Cluster/main.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cpnest
+from cpnest.model import Model
+
+
+Lmax = 4
+
+
+class VSHmodel(Model):
+    """
+    Vector Spherical Harmonic (VSH) fit to proper motions
+    """
+    
+    def __init__(self):
+    
+        names = []
+        bounds = []
+        
+        for Q in ['E', 'B']:
+            for l in np.arange(1, Lmax+1):
+                for m in np.arange(0, l+1):
+                
+                    if m==0:
+                    
+                        names += 'Re_a^'+Q+'_'+str(l)+str(m)
+                        bounds += [-1,1]
+                        
+                    else:
+                    
+                        names += 'Re_a^'+Q+'_'+str(l)+str(m)
+                        bounds += [-1,1]
+                        names += 'Im_a^'+Q+'_'+str(l)+str(m)
+                        bounds += [-1,1]
+                        
+
+    def log_likelihood(self, param):
+        return 0.0
+        
+
+# run nested sampling
+model = VSHmodel()
+nest = cpnest.CPNest(model, nlive=2048, maxmcmc=512, nthreads=8, resume=True, verbose=3)
+nest.run()
+
+# post processing run
+nest.get_nested_samples()
+nest.get_posterior_samples()
