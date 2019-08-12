3c1ad8a1759d2acbf1c75ab04e6b7bcd86d2c614
cjm96 <cjm96@users.noreply.github.com>
2019-08-12 15:43:47 +0100

    Speed up log_likelihood in main.py

--- Cluster/main.py
+++ Cluster/main.py
@@ -94,6 +94,7 @@ class VSHmodel(cpnest.model.Model):
 
     def log_likelihood(self, params):
         
+        """
         par = dict(params)
         for Q in ['E', 'B']:
             for l in np.arange(1, Lmax+1):
@@ -112,6 +113,22 @@ class VSHmodel(cpnest.model.Model):
                             par['Re_a^B_'+str(l)+'0']+0*(1j)   
                             if m==0 else   
                             par['Re_a^B_'+str(l)+str(m)]+(1j)*par['Im_a^B_'+str(l)+str(m)]
+                       for m in np.arange(-l, l+1)] for l in np.arange(1, Lmax+1)]
+        """
+        
+        vsh_E_coeffs = [ [ 
+                            param['Re_a^E_'+str(l)+'0']+0*(1j)   
+                            if m==0 else if m>0   
+                            param['Re_a^E_'+str(l)+str(m)]+(1j)*param['Im_a^E_'+str(l)+str(m)]
+                            else
+                            ((-1)**(-m)) * ( param['Re_a^E_'+str(l)+str(-m)]-(1j)*param['Im_a^E_'+str(l)+str(-m)] )
+                       for m in np.arange(-l, l+1)] for l in np.arange(1, Lmax+1)]
+        vsh_B_coeffs = [ [ 
+                            par['Re_a^B_'+str(l)+'0']+0*(1j)   
+                            if m==0 else if m>0  
+                            par['Re_a^B_'+str(l)+str(m)]+(1j)*par['Im_a^B_'+str(l)+str(m)]
+                            else
+                            ((-1)**(-m)) * ( param['Re_a^B_'+str(l)+str(-m)]-(1j)*param['Im_a^B_'+str(l)+str(-m)] )
                        for m in np.arange(-l, l+1)] for l in np.arange(1, Lmax+1)]
         
         model_pm = generate_model(vsh_E_coeffs, vsh_B_coeffs, data.positions)
