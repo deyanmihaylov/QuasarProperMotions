fba1f4b7f9f8bd5bf9872ef9a99ac7ae395d3534
cjm96 <cjm96@users.noreply.github.com>
2019-08-29 09:55:36 +0100

    Create AstrometricData.py

---
+++ AstrometricData.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy
+import csv
+
+from CoordinateTransformations import *
+from VectorSphericalHarmonicsVectorized import VectorSphericalHarmonicE, VectorSphericalHarmonicB
+from utils import *
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
