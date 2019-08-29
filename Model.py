182cde348c06c05513a20b46b310f6d180268652
Christopher Moore (School of Physics and Astronomy) <moorecj@PHY-C02YX3X.local>
2019-08-29 14:49:55 +0100

    change model par names

--- Model.py
+++ Model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def generate_model(coeffs, VSH_bank):
+def generate_model(coeffs, basis):
     """
     Generate model of PMs from a dictionary of a^Q_lm coefficients and VSH bank                                                                      
     """
-    return np.sum([ coeffs[name.replace("Y","a")]*VSH_bank[name] for name in VSH_bank.keys()], axis=0)
+    return np.sum([ coeffs[name.replace("Y","a")]*basis[name] for name in basis.keys()], axis=0)
