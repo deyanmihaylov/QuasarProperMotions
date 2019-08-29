c49ab840a0e4a08e3d6a80e390c7afb091041e9a
cjm96 <cjm96@users.noreply.github.com>
2019-08-29 11:50:39 +0100

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,15 +1,7 @@
 import numpy as np
 
-def generate_model ( coeffs , VSH_bank ):
-    # Generate model of PMs from a^Q_lm coefficients and VSH                                                                      
-
-    v_Q = numpy.sum ( [ numpy.sum ( [
-                        coeffs['Re[a^' + Q + '_' + str(l) + '0]'] * VSH_bank['Re[Y^' + Q + '_' + str(l) + '0]']
-                        + 2 * numpy.sum ( [
-                        coeffs['Re[a^' + Q + '_'+str(l)+str(m) + ']'] * VSH_bank['Re[Y^' + Q + '_' + str(l) + str(m) + ']']
-                        - coeffs['Im[a^'+ Q + '_'+str(l)+str(m) + ']'] * VSH_bank['Im[Y^' + Q + '_' + str(l) + str(m) + ']']
-                        for m in range ( 1 , l + 1 ) ] , axis=0 )
-                    for l in range ( 1 , c.Lmax + 1 ) ] , axis=0 )
-                for Q in [ 'E' , 'B' ] ] , axis=0 )
-
-    return v_Q
+def generate_model(coeffs, VSH_bank):
+    """
+    Generate model of PMs from a dictionary of a^Q_lm coefficients and VSH bank                                                                      
+    """
+    return np.sum([ coeffs[name.replace("Y","a")]*VSH_bank[name] for name in VSH_bank.keys()])
