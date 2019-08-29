1817325933bb04e21da7f8875816e590bee15a13
Christopher Moore (School of Physics and Astronomy) <moorecj@PHY-C02YX3X.local>
2019-08-29 15:02:17 +0100

    .

--- Sampler.py
+++ Sampler.py
@@ -5,6 +5,7 @@ import cpnest.model
 
 import Model 
 
+import AstrometricData as AD
 
 
 
@@ -34,46 +35,53 @@ def logL_quadratic(R):
   
   
   
+
 class model(cpnest.model.Model):
     """
-    model to fit to the proper motions
+    Model to fit to the proper motions
     """
 
     def __init__(self, dataset, prior_bound=1, whichlikelihood="permissive"):
-      """
-      initialise the model class
+        """
+        Initialise the model class
       
-      INPUTS
-      ------
-      prior_bound: float
-        the range of coefficients to search over [mas/yr]
-      whichlikelihood: str
-        which likelihood function to use [either "permissive" or "normal"]
-      """
-      
-      assert whichlikelihood=="permissive" or whichlikelihood=="normal", "Unrecognised likelihood option"
-      
+        INPUTS
+        ------
+        prior_bound: float
+            the range of coefficients to search over [mas/yr]
+        whichlikelihood: str
+            which likelihood function to use [either "permissive" or "normal"]
+        """
+        assert whichlikelihood=="permissive" or whichlikelihood=="normal", "Unrecognised likelihood option"
+
+        self.tol = 1.0e-5
+
         self.dataset = dataset
         self.whichlikelihood = whichlikelihood
-        self.prior_bound_aQlm = prior_bound
-        self.names = []
-        self.bounds = []
+        self.prior_bound = prior_bound
         
-        self.names += self.dataset.names
+        self.names = self.dataset.names
         
-        self.bounds += [[ -self.prior_bound , self.prior_bound ] for i in range(len(self.names))]
+        self.bounds = [[ -self.prior_bound , self.prior_bound ] for i in range(len(self.names))]
                         
-        print("Searching over the following parameters:", self.names)
+        print("Searching over the following parameters:\n", '\n'.join(self.names))
 
 
     def log_likelihood(self, params):  
-        
-        model_pm = Model.generate_model(params, self.data.basis )
-        Rvals = R_values(self.data.proper_motions, self.data.covariance_inv , model_pm)
-        Rvals = np.maximum(Rvals, tol)
+        """
+        The log-likelihood function
+        """
+        model_pm = Model.generate_model(params, self.dataset.basis)
+
+        Rvals = R_values(self.data.proper_motions, self.dataset.inv_proper_motion_error_matrix, model_pm)
+
+        Rvals = np.maximum(Rvals, self.tol)
+
         if self.whichlikelihood == "permissive":
-          log_likelihood = np.sum( logL_permissive( Rvals ) )
+            log_likelihood = np.sum( logL_permissive( Rvals ) )
+
         else:
-           log_likelihood = np.sum( logL_quadratic( Rvals ) )
-        
+            log_likelihood = np.sum( logL_quadratic( Rvals ) )
+
         return log_likelihood
+
