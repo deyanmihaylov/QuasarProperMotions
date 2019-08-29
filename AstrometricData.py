e202ceef60c84996376bb2aa7da3a0a65dba8fb5
cjm96 <cjm96@users.noreply.github.com>
2019-08-29 09:57:50 +0100

    Update AstrometricData.py

--- AstrometricData.py
+++ AstrometricData.py
@@ -2,10 +2,23 @@ import pandas
 import numpy
 import csv
 
-from CoordinateTransformations import *
-from VectorSphericalHarmonicsVectorized import VectorSphericalHarmonicE, VectorSphericalHarmonicB
-from utils import *
+import CoordinateTransformations as CT
+import VectorSphericalHarmonics as VSH
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+
+class AstrometricDataframe:
+    def __init__(self):
+        self.positions = numpy.array ([])
+                self.positions_Cartesian = numpy.array ([])
+
+                self.n_objects = 0
+
+                self.proper_motions = numpy.array ([])
+
+                self.covariance_inv = numpy.array ([])
+                
+                
