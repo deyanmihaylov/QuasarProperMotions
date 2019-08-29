9d0e24900eaf7abe1b890b5b5c63ea84d30f426c
Christopher Moore (School of Physics and Astronomy) <moorecj@PHY-C02YX3X.local>
2019-08-29 17:41:36 +0100

    .

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ catalogue_csv.close()
 data = AD.AstrometricDataframe()
 
 if args.dataset == 1:
-    data.gen_mock_data(500, eps=0.25, noise=0.1)
+    data.gen_mock_data(500, eps=0.15, noise=0.1)
 elif args.dataset == 2:
     data.load_Gaia_data("data/type2.csv")
 elif args.dataset == 3:
