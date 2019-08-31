e3795c78511d9d4b8242f3e261d7439b5ddc6460
deyan.mihaylov <d.p.mihaylov@gmail.com>
2019-08-31 23:44:43 +0200

    N quasars

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ catalogue_csv.close()
 data = AD.AstrometricDataframe(Lmax=args.Lmax)
 
 if args.dataset == 1:
-    data.gen_mock_data(500, eps=100, noise=0.1)
+    data.gen_mock_data(1000, eps=0.1, noise=0.1)
 elif args.dataset == 2:
     data.load_Gaia_data("data/type2.csv")
 elif args.dataset == 3:
