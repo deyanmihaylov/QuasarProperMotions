6a39e73ede198c4546f2c08d1ddc80910c708daf
Christopher Moore (School of Physics and Astronomy) <moorecj@bb-pg-login03.bear.cluster>
2019-09-02 19:54:29 +0100

    fix download data script paths

--- data/DownloadGaiaData.py
+++ data/DownloadGaiaData.py
@@ -8,17 +8,17 @@ query2and3 = "SELECT * FROM gaiadr2.gaia_source AS gaia WHERE gaia.frame_rotator
 
 gaia = Tap(url=gaiaURL)
 
-job2 = gaia.launch_job_async(query2, name=None, output_file="data/type2.csv",
+job2 = gaia.launch_job_async(query2, name=None, output_file="../data/type2.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
 
-job3 = gaia.launch_job_async(query3, name=None, output_file="data/type3.csv",
+job3 = gaia.launch_job_async(query3, name=None, output_file="../data/type3.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
 
-job2and3 = gaia.launch_job_async(query2and3, name=None, output_file="data/type2and3.csv",
+job2and3 = gaia.launch_job_async(query2and3, name=None, output_file="../data/type2and3.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
