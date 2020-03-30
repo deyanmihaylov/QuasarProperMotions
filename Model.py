e3b3a6ab8f79a52994142eb2a4eda5de24dfa553
Deyan Mihaylov <deyan.mihaylov@ligo.org>
2020-03-30 21:56:16 +0200

    Added docstring

--- Model.py
+++ Model.py
@@ -5,7 +5,15 @@ def generate_model(
 		basis:dict
 	):
     """
-    Generate model of PMs from a dictionary of almQ coefficients and some spherical harmonics basis
+    Generate model of proper motions from a dictionary of almQ coefficients
+    and some spherical harmonics basis
+
+    INPUTS
+    ------
+    almQ: dict
+    	Dictionary containing the vector spherical harmonics coefficients
+    basis: dict
+    	Dictionary containing the vector spherical harmonics
     """
     model = np.sum([almQ[key] * basis[key] for key in almQ.keys()], axis=0)
 
