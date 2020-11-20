5b7dd1e79d53b1f0f31d5f0bf3b0cea0e9577a14
Deyan Mihaylov <deyanmihaylov@supports-MacBook-Pro.local>
2020-11-20 18:14:08 +0100

    Fixing the download file

--- data/DownloadGaiaData.py
+++ data/DownloadGaiaData.py
@@ -1,4 +1,4 @@
-from astroquery.utils.tap.core import Tap
+from astroquery.gaia import Gaia
 
 gaiaURL = "http://gea.esac.esa.int/tap-server/tap"
 
@@ -6,19 +6,17 @@ query2 = "SELECT * FROM gaiadr2.gaia_source AS gaia WHERE gaia.frame_rotator_obj
 query3 = "SELECT * FROM gaiadr2.gaia_source AS gaia WHERE gaia.frame_rotator_object_type = 3"
 query2and3 = "SELECT * FROM gaiadr2.gaia_source AS gaia WHERE gaia.frame_rotator_object_type = 2 OR gaia.frame_rotator_object_type = 3"
 
-gaia = Tap(url=gaiaURL)
-
-job2 = gaia.launch_job_async(query2, name=None, output_file="../data/type2.csv",
+job = Gaia.launch_job_async(query2, name=None, output_file="../data/type2.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
 
-job3 = gaia.launch_job_async(query3, name=None, output_file="../data/type3.csv",
+job3 = Gaia.launch_job_async(query3, name=None, output_file="../data/type3.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
 
-job2and3 = gaia.launch_job_async(query2and3, name=None, output_file="../data/type2and3.csv",
+job2and3 = Gaia.launch_job_async(query2and3, name=None, output_file="../data/type2and3.csv",
                              output_format="csv", verbose=False,
                              dump_to_file=True, background=False,
                              upload_resource=None, upload_table_name=None)
