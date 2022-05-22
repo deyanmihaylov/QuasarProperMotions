ca4246d428d2efd632447edf0363186df3c1c81a
Deyan Mihaylov <dmihaylov@hypatia1.hypatia.local>
2022-05-22 21:24:43 +0200

    Add type hints

--- vector_spherical_harmonics.py
+++ vector_spherical_harmonics.py
@@ -1,37 +1,35 @@
-######################################
-### The Vector Spherical Harmonics ###
-######################################
+"""
+These functions are normalised such that
+< (Y^l_m)^E | (Y^l_m)^E > = < (Y^l_m)^B | (Y^l_m)^B > = 1
 
-# These functions are normalised such that
-# < (Y^l_m)^E | (Y^l_m)^E > = < (Y^l_m)^B | (Y^l_m)^B > = 1
-#
-# They agree with the following Mathematica functions
-#
-#  YE[l_, m_, th_, ph_] := Block[{eth, eph, pol, az, DYDTH, DYDPH},
-#  eth = {Cos[th] Cos[ph], Cos[th] Sin[ph], -Sin[th]};
-#  eph = {-Sin[ph], Cos[ph], 0};
-#  DYDTH = D[SphericalHarmonicY[l, m, pol, ph], pol] /. pol -> th;
-#  DYDPH = D[SphericalHarmonicY[l, m, th, az], az] /. az -> ph;
-#  Return[(DYDTH eth + DYDPH eph/Sin[th])/Sqrt[l (l + 1)]];]
-#
-#  YB[l_, m_, th_, ph_] := Block[{eth, eph, pol, az, DYDTH, DYDPH, grad, n},
-#  eth = {Cos[th] Cos[ph], Cos[th] Sin[ph], -Sin[th]};
-#  eph = {-Sin[ph], Cos[ph], 0};
-#  DYDTH = D[SphericalHarmonicY[l, m, pol, ph], pol] /. pol -> th;
-#  DYDPH = D[SphericalHarmonicY[l, m, th, az], az] /. az -> ph;
-#  n = {Sin[th] Cos[ph], Sin[th] Sin[ph], Cos[th]};
-#  grad = (DYDTH eth + DYDPH eph/Sin[th]);
-#  Return[Cross[n,grad]/Sqrt[l (l + 1)]];]
+They agree with the following Mathematica functions
+
+ YE[l_, m_, th_, ph_] := Block[{eth, eph, pol, az, DYDTH, DYDPH},
+ eth = {Cos[th] Cos[ph], Cos[th] Sin[ph], -Sin[th]};
+ eph = {-Sin[ph], Cos[ph], 0};
+ DYDTH = D[SphericalHarmonicY[l, m, pol, ph], pol] /. pol -> th;
+ DYDPH = D[SphericalHarmonicY[l, m, th, az], az] /. az -> ph;
+ Return[(DYDTH eth + DYDPH eph/Sin[th])/Sqrt[l (l + 1)]];]
+
+ YB[l_, m_, th_, ph_] := Block[{eth, eph, pol, az, DYDTH, DYDPH, grad, n},
+ eth = {Cos[th] Cos[ph], Cos[th] Sin[ph], -Sin[th]};
+ eph = {-Sin[ph], Cos[ph], 0};
+ DYDTH = D[SphericalHarmonicY[l, m, pol, ph], pol] /. pol -> th;
+ DYDPH = D[SphericalHarmonicY[l, m, th, az], az] /. az -> ph;
+ n = {Sin[th] Cos[ph], Sin[th] Sin[ph], Cos[th]};
+ grad = (DYDTH eth + DYDPH eph/Sin[th]);
+ Return[Cross[n,grad]/Sqrt[l (l + 1)]];]
+ """
 
 import numpy as np
+import numpy.typing as npt
 from scipy.special import lpmv
 from math import factorial
-import functools
 
 def normalised_associated_Legendre_polynomial(
-    l,
-    m,
-    x,
+    l: int,
+    m: int,
+    x: float,
 ):
     """
     The Normalised Associated Legendre Polynomials
@@ -59,9 +57,9 @@ def normalised_associated_Legendre_polynomial(
     return norm * legendre
 
 def scalar_spherical_harmonic_Y(
-    l,
-    m,
-    n,
+    l: int,
+    m: int,
+    n: npt.NDArray,
 ):
     """
     The Scalar Spherical Harmonics, Y^l_m(n)
@@ -89,9 +87,9 @@ def scalar_spherical_harmonic_Y(
     )
 
 def vector_spherical_harmonic_E(
-    l,
-    m,
-    n,
+    l: int,
+    m: int,
+    n: npt.NDArray,
 ):
     """
     The Gradient Vector Spherical Harmonics, (YE)^l_m(n).
@@ -170,9 +168,9 @@ def vector_spherical_harmonic_E(
     return v_E
 
 def vector_spherical_harmonic_B(
-    l,
-    m,
-    n,
+    l: int,
+    m: int,
+    n: npt.NDArray,
 ):
     """
     The Curl Vector Spherical Harmonics, (YB)^l_m(n).
@@ -231,7 +229,8 @@ def vector_spherical_harmonic_B(
             + c2 * normalised_associated_Legendre_polynomial (l, m + 1, x)
         ) * np.exp(1j * m * phi)
     
-    # The Derivative Of The Spherical Harmonic Function Y^l_m WRT To Phi Divided By Sin(Theta)
+    # The Derivative Of The Spherical Harmonic Function Y^l_m WRT To Phi
+    # Divided By Sin(Theta)
     c1 = np.sqrt((2 * l + 1) * (l - m + 1) * (l - m + 2) / (2 * l + 3))
     c2 = np.sqrt((2 * l + 1) * (l + m + 1) * (l + m + 2) / (2 * l + 3))
     dY_dphi_over_sin_theta = - ((1 / 2) * (
@@ -250,9 +249,9 @@ def vector_spherical_harmonic_B(
     return v_B
 
 def real_vector_spherical_harmonic_E(
-    l,
-    m,
-    n,
+    l: int,
+    m: int,
+    n: npt.NDArray,
 ):
     """
     The Real Gradient Vector Spherical Harmonics, (YE)^l_m(n).
@@ -285,9 +284,9 @@ def real_vector_spherical_harmonic_E(
         )
 
 def real_vector_spherical_harmonic_B(
-    l,
-    m,
-    n,
+    l: int,
+    m: int,
+    n: npt.NDArray,
 ):
     """
     The Real Curl Vector Spherical Harmonics, (YB)^l_m(n).
